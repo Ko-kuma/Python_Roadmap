01808b5cddd9680425cc6cd89b1e5d17d69491a6
Ko-kuma <fxzkrqtzhf@privaterelay.appleid.com>
2025-12-06 22:44:16 +0900

    Day 5&6 : Python 리스트(List) 자료형 학습 및 For문 결합 실습 완료(12월5일회식으로 인한밀린 학습 처리)

---
+++ 01_basic_python/day_5_test.py
@@ -0,0 +1,8 @@
+shopping_list = ["계란", "우유", "라면", "커피"]
+
+print("------------------")
+
+for item in shopping_list:
+    #item변수에 "계란", "우유", "라면"이 순서대로 들어갑니다.
+    print(f"{item}구매하기")
+print("---쇼핑 끝---")
