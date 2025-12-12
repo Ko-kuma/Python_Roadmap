ffb9886aec11d8ac7ae5657b6ce98770945a756b
Ko-kuma <fxzkrqtzhf@privaterelay.appleid.com>
2025-12-12 23:15:19 +0900

    Day12 : Input and Exception Hanling Practic

---
+++ 01_basic_python/week2_structure/day_12/day_12_input.py
@@ -0,0 +1,12 @@
+print("---프로그램 시작---")
+
+name=input("이름을 입력하세요 : ")
+print(f"반갑습니다{name}님!")
+
+age =input("나이를 입력하세요")
+print(f"내년에는 {int(age)+1}살이 되시네요.")
+
+#또 다른 방식으로 정수를 입력하면
+
+year = int(input("년도를 입력 하세요 : "))
+print(f"올해는 {year}년 입니다.")
