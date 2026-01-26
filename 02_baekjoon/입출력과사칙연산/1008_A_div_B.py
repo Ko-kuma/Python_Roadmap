070cc7a5710f5475b68dcd5d449e63a1addb14a4
Ko-kuma <fxzkrqtzhf@privaterelay.appleid.com>
2026-01-27 00:14:16 +0900

    사칙연산

---
+++ 02_baekjoon/입출력과사칙연산/1008_A_div_B.py
@@ -0,0 +1,7 @@
+'''
+a = float(input("값1"))
+b = float(input("값2"))
+print(a / b)
+'''
+a, b = map(float, input().split())
+print(a / b)
---
+++ 02_baekjoon/입출력과사칙연산/10869_Arithmetic.py
@@ -0,0 +1,7 @@
+'''
+a = 7
+b = 3
+print(f"{a + b}\n{a - b}\n{a * b}\n{a // b}\n{a % b}")
+'''
+a, b = map(int, input().split())
+print(f"{a + b}\n{a - b}\n{a * b}\n{a // b}\n{a % b}")
